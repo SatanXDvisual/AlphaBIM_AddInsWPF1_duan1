afc0ab5b16e49e38dd4d0fbbea195c17dc2fa350
WOV 2024 <nguyenquang.vsi@gmail.com>
2025-07-02 10:01:25 +0700

    Add project files.

---
+++ AlphaBIM_AddInsWPF1_duan1/AlphaBIM_AddInsWPF1_duan1ViewModel.cs
@@ -0,0 +1,58 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+#endregion
+
+namespace AlphaBIM
+{
+    public class AlphaBIM_AddInsWPF1_duan1ViewModel : ViewModelBase
+    {
+        public AlphaBIM_AddInsWPF1_duan1ViewModel(UIDocument uiDoc)
+        {
+            // Khởi tạo sự kiện(nếu có) | Initialize event (if have)
+
+            // Lưu trữ data từ Revit | Store data from Revit
+            UiDoc = uiDoc;
+            Doc = UiDoc.Document;
+
+            // Khởi tạo data cho WPF | Initialize data for WPF
+            Initialize();
+
+            // Get setting(if have)
+
+        }
+
+        private void Initialize()
+        {
+
+        }
+
+        #region public property
+
+        public UIDocument UiDoc;
+        public Document Doc;
+
+        #endregion public property
+
+        #region private variable
+
+        private double _percent;
+
+        #endregion private variable
+
+        // Các method khác viết ở dưới đây | Other methods written below
+
+
+    }
+}
